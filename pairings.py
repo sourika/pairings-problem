892bf92e7e84b9d9f26ac092c9da84714fd372a3
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-12-17 00:12:16 -0800

    Implement find_pairs function to generate unique pairs from a space-separated string of numbers

--- pairings.py
+++ pairings.py
@@ -1,8 +1,22 @@
 def find_pairs(num_string):
-  # Write your solution here!
-  pass
+  pairs = set()
+  lst_num = num_string.split(" ") 
+  new_lst = []                    
+  for num in lst_num:
+    new_lst.append(int(num))
 
 
+  for i in range(len(new_lst)):
+    for j in range(i + 1, len(new_lst)):      
+      if new_lst[i] < new_lst[j]:
+        pairs.add((new_lst[i], new_lst[j]))
+      else:
+        pairs.add((new_lst[j], new_lst[i]))  
+
+  return pairs
+
+
+find_pairs("7 3 99")
 # Test cases
 assert find_pairs("7 3 99") == {(7, 99), (3, 7), (3, 99)}
 assert find_pairs("2 1") == {(1, 2)}
